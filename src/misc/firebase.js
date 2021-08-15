bd319943439ebeb854c6627c69374494d1bfb97e
NikhilGupta <nikhilg398@gmail.com>
2021-08-15 15:48:50 +0530

    added signin with fb/google

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,12 +1,18 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
   apiKey: 'AIzaSyAfzqQLTTaXKF1_8tzx0LDvp4RaKCtI1us',
   authDomain: 'chat-web-app-6aea3.firebaseapp.com',
+  databaseURL:
+    'https://chat-web-app-6aea3-default-rtdb.asia-southeast1.firebasedatabase.app',
   projectId: 'chat-web-app-6aea3',
   storageBucket: 'chat-web-app-6aea3.appspot.com',
   messagingSenderId: '988300422009',
   appId: '1:988300422009:web:4f5cd1b3e46b7f01f65da8',
 };
 
-firebase.initializeApp(config);
+const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
