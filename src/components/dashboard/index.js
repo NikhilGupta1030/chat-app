4a592bccc1d5b004dd36e3cc1241d571b251a419
NikhilGupta <nikhilg398@gmail.com>
2021-08-16 19:43:46 +0530

    created reusable input

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,10 +1,14 @@
-/* eslint-disable react/self-closing-comp */
 import React from 'react';
-import { Button, Drawer } from 'rsuite';
+import { Button, Divider, Drawer } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
+import EditableInput from '../EditableInput';
 
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
+
+  const onSave = async newData => {
+    console.log(newData);
+  };
 
   return (
     <>
@@ -14,6 +18,13 @@ const Dashboard = ({ onSignOut }) => {
 
       <Drawer.Body>
         <h3>Hey, {profile.name}</h3>
+        <Divider />
+        <EditableInput
+          name="nickname"
+          initialValue={profile.name}
+          onSave={onSave}
+          label={<h6 className="mb-2">Nickname</h6>}
+        />
       </Drawer.Body>
 
       <Drawer.Footer>
