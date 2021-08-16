b59f9457b6b3555cfcd93790e5e02feba224a354
NikhilGupta <nikhilg398@gmail.com>
2021-08-16 16:45:44 +0530

    added signout and name

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,7 +1,28 @@
+/* eslint-disable react/self-closing-comp */
 import React from 'react';
+import { Button, Drawer } from 'rsuite';
+import { useProfile } from '../../context/profile.context';
 
-const Dashboard = () => {
-  return <div>Hey, Whats up dude!!</div>;
+const Dashboard = ({ onSignOut }) => {
+  const { profile } = useProfile();
+
+  return (
+    <>
+      <Drawer.Header>
+        <Drawer.Title>Dashboard</Drawer.Title>
+      </Drawer.Header>
+
+      <Drawer.Body>
+        <h3>Hey, {profile.name}</h3>
+      </Drawer.Body>
+
+      <Drawer.Footer>
+        <Button block color="red" onClick={onSignOut}>
+          Sign out
+        </Button>
+      </Drawer.Footer>
+    </>
+  );
 };
 
 export default Dashboard;
