2a0ebd4dd706dfd59245ca0ab3532934b6eed15d
NikhilGupta <nikhilg398@gmail.com>
2021-08-18 21:22:19 +0530

    created room context

--- src/pages/Home/Chat.js
+++ src/pages/Home/Chat.js
@@ -5,6 +5,7 @@ import ChatTop from '../../components/chat-window/top';
 import Messages from '../../components/chat-window/messages';
 import ChatBottom from '../../components/chat-window/bottom';
 import { useRooms } from '../../context/rooms.context';
+import { CurrentRoomProvider } from '../../context/current-room.context';
 
 const Chat = () => {
   const { chatId } = useParams();
@@ -20,8 +21,15 @@ const Chat = () => {
     return <h6 className="text-center mt-page">Chat {chatId} not found</h6>;
   }
 
+  const { name, description } = currentRoom;
+
+  const currentRoomData = {
+    name,
+    description,
+  };
+
   return (
-    <>
+    <CurrentRoomProvider data={currentRoomData}>
       <div className="chat-top">
         <ChatTop />
       </div>
@@ -31,7 +39,7 @@ const Chat = () => {
       <div className="chat-bottom">
         <ChatBottom />
       </div>
-    </>
+    </CurrentRoomProvider>
   );
 };
 
