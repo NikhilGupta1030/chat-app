c4431f065146fbd8d177be4f092ba4f5b9c3601b
NikhilGupta <nikhilg398@gmail.com>
2021-08-20 01:09:51 +0530

    added prog hover

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,4 +1,5 @@
-import { useCallback, useState, useEffect } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useCallback, useState, useEffect, useRef } from 'react';
 import { database } from './firebase';
 
 export function useModalState(defaultValue = false) {
@@ -45,4 +46,26 @@ export function usePresence(uid) {
     };
   }, [uid]);
   return presence;
+}
+
+export function useHover() {
+  const [value, setValue] = useState(false);
+  const ref = useRef(null);
+  const handleMouseOver = () => setValue(true);
+  const handleMouseOut = () => setValue(false);
+  useEffect(
+    () => {
+      const node = ref.current;
+      if (node) {
+        node.addEventListener('mouseover', handleMouseOver);
+        node.addEventListener('mouseout', handleMouseOut);
+      }
+      return () => {
+        node.removeEventListener('mouseover', handleMouseOver);
+        node.removeEventListener('mouseout', handleMouseOut);
+      };
+    },
+    [ref.current] // Recall only if ref changes
+  );
+  return [ref, value];
 }
