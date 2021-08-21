6a501a4f016a23d97235560ca16280b273b6013c
NikhilGupta <nikhilg398@gmail.com>
2021-08-22 00:57:53 +0530

    grouped msgs by date

--- src/misc/helpers.js
+++ src/misc/helpers.js
@@ -48,3 +48,17 @@ export async function getUserUpadtes(userId, keyToUpdate, value, db) {
 
   return updates;
 }
+
+export function groupBy(array, groupingKeyFn) {
+  return array.reduce((result, item) => {
+    const groupingKey = groupingKeyFn(item);
+
+    if (!result[groupingKey]) {
+      result[groupingKey] = [];
+    }
+
+    result[groupingKey].push(item);
+
+    return result;
+  }, {});
+}
