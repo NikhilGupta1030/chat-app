5afaf65422b61bbd9cb42d6eac6967cc2e2d3e77
NikhilGupta <nikhilg398@gmail.com>
2021-08-22 00:08:38 +0530

    added audio feature and listen them

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -1,19 +1,42 @@
 import React, { memo } from 'react';
-import { Button } from 'rsuite';
 import TimeAgo from 'timeago-react';
-import { useCurrentRoom } from '../../../context/current-room.context';
-import { useHover, useMediaQuery } from '../../../misc/custom-hooks';
-import { auth } from '../../../misc/firebase';
-import PresenceDot from '../../PresenceDot';
+import { Button } from 'rsuite';
 import ProfileAvatar from '../../ProfileAvatar';
-import IconBtnControl from './IconBtnControl';
 import ProfileInfoBtnModal from './ProfileInfoBtnModal';
+import PresenceDot from '../../PresenceDot';
+import { useCurrentRoom } from '../../../context/current-room.context';
+import { auth } from '../../../misc/firebase';
+import { useHover, useMediaQuery } from '../../../misc/custom-hooks';
+import IconBtnControl from './IconBtnControl';
+import ImgBtnModal from './ImgBtnModal';
+
+const renderFileMessage = file => {
+  if (file.contentType.includes('image')) {
+    return (
+      <div className="height-220">
+        <ImgBtnModal src={file.url} fileName={file.name} />
+      </div>
+    );
+  }
+
+  if (file.contentType.includes('audio')) {
+    return (
+      // eslint-disable-next-line jsx-a11y/media-has-caption
+      <audio controls>
+        <source src={file.url} type="audio/mp3" />
+        Your browser does not support the audio element.
+      </audio>
+    );
+  }
+
+  return <a href={file.url}>Download {file.name}</a>;
+};
 
 const MessageItem = ({ message, handleAdmin, handleLike, handleDelete }) => {
-  const { author, createdAt, text, likes, likeCount } = message;
+  const { author, createdAt, text, file, likes, likeCount } = message;
 
   const [selfRef, isHovered] = useHover();
-  const isMobile = useMediaQuery('(max-width:992px)');
+  const isMobile = useMediaQuery('(max-width: 992px)');
 
   const isAdmin = useCurrentRoom(v => v.isAdmin);
   const admins = useCurrentRoom(v => v.admins);
@@ -23,7 +46,6 @@ const MessageItem = ({ message, handleAdmin, handleLike, handleDelete }) => {
   const canGrantAdmin = isAdmin && !isAuthor;
 
   const canShowIcons = isMobile || isHovered;
-
   const isLiked = likes && Object.keys(likes).includes(auth.currentUser.uid);
 
   return (
@@ -47,24 +69,18 @@ const MessageItem = ({ message, handleAdmin, handleLike, handleDelete }) => {
           className="p-0 ml-1 text-black"
         >
           {canGrantAdmin && (
-            <Button
-              block
-              onClick={() => {
-                handleAdmin(author.uid);
-              }}
-              color="blue"
-            >
+            <Button block onClick={() => handleAdmin(author.uid)} color="blue">
               {isMsgAuthorAdmin
                 ? 'Remove admin permission'
                 : 'Give admin in this room'}
             </Button>
           )}
         </ProfileInfoBtnModal>
-
         <TimeAgo
           datetime={createdAt}
           className="font-normal text-black-45 ml-2"
         />
+
         <IconBtnControl
           {...(isLiked ? { color: 'red' } : {})}
           isVisible={canShowIcons}
@@ -78,12 +94,14 @@ const MessageItem = ({ message, handleAdmin, handleLike, handleDelete }) => {
             isVisible={canShowIcons}
             iconName="close"
             tooltip="Delete this message"
-            onClick={() => handleDelete(message.id)}
+            onClick={() => handleDelete(message.id, file)}
           />
         )}
       </div>
+
       <div>
-        <span className="word-breal-all">{text}</span>
+        {text && <span className="word-breal-all">{text}</span>}
+        {file && renderFileMessage(file)}
       </div>
     </li>
   );
